c29be6a93d80145eb1e5de64f40db72b605e1180
jcr04 <joaocarlosreboucas15@gmail.com>
2023-12-30 16:30:04 -0300

    add project

---
+++ ITicket.Domain/Entities/Event.cs
@@ -0,0 +1,28 @@
+﻿namespace ITicket.Domain.Entities
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public string? Location { get; set; }
+
+        public decimal TicketPrice { get; set; }
+
+        public int TotalTicketsAvailable { get; set; }
+
+        public int TicketsReserved { get; set; }
+
+        // Outros campos relacionados ao evento
+
+        // Relacionamento com as reservas
+        public List<Reservation>? Reservations { get; set; }
+    }
+}
---
+++ ITicket.Domain/Entities/Reservation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ITicket.Domain.Entities
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+
+        public int EventId { get; set; }
+
+        public string? ReservationCode { get; set; }
+
+        public string? ReservedBy { get; set; }
+
+        public int NumberOfTickets { get; set; }
+
+        public DateTime ReservationTime { get; set; }
+
+        // Relacionamento com o evento
+        public Event? Event { get; set; }
+    }
+}
